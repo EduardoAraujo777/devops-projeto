3166c27726655cc213217d040e17238923c0bfd8
Eduardo Araujo <eduardo@email.com>
2026-04-13 16:21:53 -0300

    ignore README in CI pipeline

--- src/app.js
+++ src/app.js
@@ -3,8 +3,31 @@ console.log("INICIOU O ARQUIVO");
 const express = require('express');
 const app = express();
 
+/**
+ * ROTA PRINCIPAL
+ * Retorna status da API
+ */
 app.get('/', (req, res) => {
-  res.send('API rodando 🚀');
+  res.json({
+    status: "ok",
+    message: "API DevOps rodando com Docker 🚀",
+    version: "1.0.0"
+  });
 });
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+/**
+ * HEALTH CHECK
+ * Usado em sistemas reais pra monitorar se a API está viva
+ */
+app.get('/health', (req, res) => {
+  res.json({
+    status: "healthy"
+  });
+});
+
+/**
+ * START SERVER
+ */
+app.listen(3000, "0.0.0.0", () => {
+  console.log("🚀 API rodando na porta 3000");
+});
